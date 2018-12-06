99519cbd64727dfd1da8b4c6b3d40f1de708c4f7
fengxuanzhen <1071277710@qq.com>
2018-12-06 16:03:49 +0800

    d

--- crawlData/practice.py
+++ crawlData/practice.py
@@ -19,9 +19,9 @@ SEQUENCE_LENGTH = 250
 EMBEDDING_DIM = 10
 HIDDEN_SIZE = 150
 ATTENTION_SIZE = 50
-KEEP_PROB = 0.8
+KEEP_PROB = 0.5
 BATCH_SIZE = 256
-NUM_EPOCHS = 30  # Model easily overfits without pre-trained words embeddings, that's why train for a few epochs
+NUM_EPOCHS = 50  # Model easily overfits without pre-trained words embeddings, that's why train for a few epochs
 DELTA = 0.5
 MODEL_PATH = './model'
 
